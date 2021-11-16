9b6c491eb7ed8c998d29fed7a4f4ed662025964d
Ana Maria <anamaurizio@hotmail.com>
2021-11-16 19:15:29 -0300

    alterações feitas no projeto

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,97 @@
 <template>
-  <div class="about">
+  <div class="Login">
     <h1>Login</h1>
+    <v-card class="justify-center">
+      <form action="/action_page.php"></form>
+      <input
+        type="e-mail"
+        id="fname"
+        class="input"
+        name="fname"
+        placeholder="E-mail"
+      /><br /><br />
+      <input
+        type="password"
+        id="lname"
+        class="input"
+        name="lname"
+        placeholder="Senha"
+        autocomplete="on"
+      /><br /><br />
+    </v-card>
+    <input
+      type="checkbox"
+      id="lembrardemim"
+      name="lembrardemim"
+      onclick="autocomplete(on)"
+    />
+    <label for="lembrardemim">Lembrar de mim </label>
+    <p id="esqueceu-senha">
+      Esqueceu sua senha?
+      <a href="http://localhost:8080/RedefinirSenha.vue#/redefinir-senha"
+        >Clique aqui</a
+      >.
+    </p>
+    <div class="btn-login">
+      <button>Entrar</button>
+    </div>
+    <div>
+      <p id="rodape-pet">
+        Perdeu ou encontrou um pet e quer divulgá-lo? Faça seu cadastro
+        <a href="http://localhost:8080/cadastro.html#/cadastro">aqui</a>.
+      </p>
+    </div>
   </div>
 </template>
 <script></script>
-<style></style>
+<style>
+#body-login {
+  text-align: center;
+  position: absolute;
+  width: 70%;
+  left: 15%;
+  right: 15%;
+  height: 80%;
+}
+
+h1 {
+  font-size: 50px;
+  margin-bottom: 50px;
+  position: center;
+}
+
+.input {
+  width: 20rem;
+  height: 1.2rem;
+  margin-left: 490px;
+}
+
+#div-botao {
+  position: absolute;
+  top: 500px;
+  margin-left: 550px;
+  left: 200px;
+}
+
+#lembrardemim {
+  margin-bottom: 2rem;
+  margin-left: 550px;
+}
+
+#esqueceu-senha {
+  margin-left: 490px;
+}
+
+#btn-entrar {
+  position: absolute;
+  top: 500px;
+  left: 200px;
+}
+
+.button:hover {
+  position: absolute;
+  top: 500px;
+  left: 200px;
+  background-color: rgb(18, 187, 94);
+}
+</style>
--- src/pages/RedefinirSenha.vue
+++ src/pages/RedefinirSenha.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
     <h1>Redefinir Senha</h1>
+    <input type="email" id="emailNumero" name="emailNumero" size="30" placeholder="Insira seu e-mail"><br>
+      <p>- ou -</p>
+      <input type="text" id="perguntaSeg" name="perguntaSeg" size="30" placeholder="Resposta da pergunta de seguranca"><br><br>
+      <div id="botao">
+        <input type="submit" class='button' id='button' value="Enviar" onclick="openMessage()">
+        <p><a href="http://localhost:8080/Cadastro.Vue#/cadastro">Criar nova conta</a></p>
+      </div>
   </div>
 </template>
 <script></script>
