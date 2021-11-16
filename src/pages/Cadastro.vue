8dea27f80657fb5951bee851422f473c280ab64b
Stephani Silvia Zatta <stephani.zatta@ambevtech.com.br>
2021-11-16 19:52:00 -0300

    ainda testando o html e css

--- src/pages/Cadastro.vue
+++ src/pages/Cadastro.vue
@@ -1,7 +1,92 @@
 <template>
-  <div class="about">
-    <h1>Cadastro</h1>
-  </div>
+  <div id="about">  
+  
+    <form id="form-cadastro">
+      <fieldset>                               
+        <h1>Cadastre-se</h1>
+        <input v-model="nome" name="nome" type="text" class="inputs" id="nome" placeholder="Nome completo">  
+        <input v-model="email" type="email" class="inputs" id="email" placeholder="E-mail"> <br>   
+        <input v-model="celular" type="tel" class="inputs" id="celular" placeholder="Celular"> <br>    
+        <input v-model="endereco" type="text" class="inputs" id="endereco" placeholder="Endereço">   
+        <input v-model="nr_residencial" type="number" class="inputs" id="nr-residencia" placeholder="Nº"> <br>
+        <input v-model="complemento" type="text" class="inputs" id="complemento" placeholder="Complemento"> <br>
+        <input v-model="senha" type="password" class="inputs" id="senha" placeholder="Senha"> <br>
+        <input v-model="repita_senha" type="password" class="inputs" id="repita-senha" placeholder="Repita sua senha"> <br>
+        <select name="pergunta-seguranca" class="inputs" id="pergunta-seguranca">
+            <option value="nm-primeiro-pet">Nome do seu primeiro pet</option>    
+            <option value="anos-primeiro-pet">Com quantos anos teve seu primeiro pet</option>    
+            <option value="quantos-pets-teve">Quantos pets você já teve</option>    
+            <option value="raca-ultimo-pet">Raça do seu último pet</option>    
+            <option value="primeiro-pet">Qual foi seu primeiro pet</option>    
+            <option value="pet-favorito">Qual seu pet favorito</option>    
+        </select> <br>
+        <input v-model="resposta" type="text" class="inputs" id="resposta" placeholder="Resposta"> <br><br> 
+      
+        <div id="div-botoes">
+          <input type="submit" class="botoes" id="btn-limpar" name="btn-limpar" value="Limpar" @click="limparCampos()">
+          <input type="submit" class="botoes" id="btn-enviar" name="btn-enviar" value="Enviar" @click="verificaCampos()">
+        </div>
+      </fieldset>
+    </form>
+  </div> 
 </template>
-<script></script>
-<style></style>
+
+<style>
+  fieldset{
+    padding-bottom: 2rem;
+  }
+
+  #form-cadastro{
+    width:70%;
+    left: 15%;
+    right: 15%;
+    text-align: center;
+    position: absolute;
+  }
+
+  h1{
+    font-size: 2.5rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+  }
+
+  #endereco{
+    width: 390px;
+  }
+  #nr-residencia{
+    width: 80px;
+  }
+  #pergunta-seguranca{
+    width: 488px;
+    height: 1.5rem;
+  }
+
+  #div-botoes{
+    padding-top: 1.5rem;
+    padding-bottom: 2rem;
+    margin-right: 1rem;
+  }
+
+  .botoes{
+    height: 2rem;
+    width: 5rem;
+    margin-right: 13px;
+    float: right;
+  }
+
+  .botoes:hover {
+    background-color: rgb(197, 197, 197);
+  }
+
+  .inputs{
+    width: 30rem;
+    height: 1.2rem;
+    box-shadow: 15px;
+    border: 80px;
+    outline: 15%;
+  }
+</style>
+
+<script>
+  
+</script>
